69a56d455b2e901d2afec6515cb49046944a94cc
Julia Miodyńska <julia.miodynska@gmail.com>
2026-02-03 11:15:48 +0100

    Remove tabs

--- app/index.tsx
+++ app/index.tsx
@@ -7,10 +7,7 @@ import { ThemedView } from '@/components/themed-view';
 export default function ModalScreen() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
+      <ThemedText type="title">This is a carousel</ThemedText>
     </ThemedView>
   );
 }
